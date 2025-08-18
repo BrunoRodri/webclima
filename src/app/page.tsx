91790568428fbc7c0ff803e1deb715820702bed9
BrunoRodri <fidbruno@hotmail.com>
2025-08-18 19:26:04 -0300

    feat: add first weather info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,123 @@
-import Navbar from "@/components/Navbar";
+'use client';
+import Container from '@/components/Container';
+import Navbar from '@/components/Navbar';
+import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelcius';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { format, parseISO } from 'date-fns';
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=joao%20pessoa&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    }
+  });
+
+  const firstData = data?.list[0];
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+  if (error) return <div>Error fetching data</div>;
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4">
+        <section className='space-y-4'>
+          <div className='space-y-2'>
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p> {format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
+              <p className="text-lg">
+                {' '}
+                {format(parseISO(firstData?.dt_txt ?? ''), '(dd.MM.yyyy)')}
+              </p>
+            </h2>
+            <Container className="gap-10 x-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°C
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  Feels like{' '}
+                  {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°C
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}°C ↓
+                  </span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°C ↑
+                  </span>
+                </p>
+              </div>
+            </Container>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
+'use client';
 import React from 'react';
 import { MdLocationOn, MdMyLocation, MdWbSunny } from 'react-icons/md';
 import SearchBox from './SearchBox';
 
-type Props = {};
+type Props = { location?: string };
 
 export default function Navbar({}: Props) {
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-        <p className="flex items-center justify-center gap-2">
+        <span className="flex items-center justify-center gap-2">
           <h2 className="text-gray-500 text-3xl">WebClima</h2>
           <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
-        </p>
+        </span>
         {/* */}
         <section className="flex items-center gap-2">
           <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'/>
