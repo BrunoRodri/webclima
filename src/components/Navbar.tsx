7905a07dcc62f80b1b00a238199f21a3163dd24a
BrunoRodri <fidbruno@hotmail.com>
2025-08-26 17:29:15 -0300

    style: fix suggestion box style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -193,7 +193,7 @@ function SuggestionBox({
   return (
     <>
       {((showSuggestions && suggestions.length > 0) || error) && (
-        <ul className="mb-4 bg-blue-400 absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
+        <ul className="mb-4 bg-blue-400 absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2 z-50">
           {error && suggestions.length < 1 && (
             <li className="text-red-500 p-1 "> {error}</li>
           )}
